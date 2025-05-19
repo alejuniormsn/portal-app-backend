8dd2dd02bbfdc411c1324427c6c8a8e43917fea6
Alexandre Jr Miranda <49948786+alejuniormsn@users.noreply.github.com>
2025-05-19 09:58:21 -0300

    Update index.ts

--- src/environment/index.ts
+++ src/environment/index.ts
@@ -1,14 +1,14 @@
 export const env = {
   APP: "ETT",
   PORT: "3001",
-  JWT_SECRET: "portal_adm_ett",
+  JWT_SECRET: "SECRET",
 
   // Integração com Banco de Dados Oracle (Globus)
   BASE_URL_API_GLOBUS: "http://192.168.0.21:8000/globus",
 
   // Servidor de E-Mail Ubuntu-Server
   BASE_URL_API_SENDMAIL: "http://192.168.0.21:3030",
-  SENDMAIL_AUTH: "Basic dGkwNDpFdHRAMjAyNA==",
+  SENDMAIL_AUTH: "Basic SECRET",
 
   // Serviço LOGS
   LOG_PATH: "/home/dev/errorLogs",
